9c7d66ece2bb4e38d12d82bc1c9ef917165d09fe
goushixue <37802226+goushixue@users.noreply.github.com>
2019-08-01 20:37:25 +0800

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -5,7 +5,8 @@ options(BioC_mirror="https://mirrors.tuna.tsinghua.edu.cn/bioconductor")
 
 
 # install packages from CRAN
-Packages <- c('DT', 'shiny', 'reshape2', 'stringi', 'ggplot2', 'magrittr', 'shinydashboard', 'shinycssloaders', 'ggseqlogo')
+Packages <- c('DT', 'shiny', 'reshape2', 'stringi', 'ggplot2', 'markdown',
+              'magrittr', 'shinydashboard', 'shinycssloaders', 'ggseqlogo')
 install.packages(Packages)
 
 # install packages from bioconductor
